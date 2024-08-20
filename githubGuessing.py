0af1474cf3c512caa56db56ba4bacf395d058606
jasengan86 <71538904+jasengan86@users.noreply.github.com>
2024-08-19 23:52:04 -0700

    Update githubGuessing.py

--- githubGuessing.py
+++ githubGuessing.py
@@ -1,9 +1,8 @@
 from random import randint as r
 
-
 print("="*50)
-print("""		Guess the Secret Number!
-You start out with 3 lives and you lose one with each wrong guess!""")
+print("""        Guess the Secret Number!
+You start out with 10 lives and you lose one with each wrong guess!""")
 print("="*50)
 print()
 
@@ -12,26 +11,28 @@ def game():
     num_lives = 10
     
     while num_lives > 0:
-    print(f"Lives remaining: {num_lives}")
+        print(f"Lives remaining: {num_lives}")
 
-    try:
-        guess = int(input('Please guess the secret number between 1 and 30: '))
-    except ValueError:
-        print('Invalid range. Please choose 1 - 30')
-        continue
+        try:
+            guess = int(input('Please guess the secret number between 1 and 30: '))
+        except ValueError:
+            print('Invalid input. Please enter a number between 1 and 30.')
+            continue
 
-    if guess < 0 or guess > 30:
-        print('Invalid range. Please choose 1 - 30')
-        continue
-    elif guess > number:
-        print('Too high!')
-    elif guess < number:
-        print('Too low!')
-    else:
-        print('Congratulations! You have guessed the secret number: ', number)
-        break
-    num_lives -= 1
+        if guess < 1 or guess > 30:
+            print('Invalid range. Please choose 1 - 30')
+            continue
+        elif guess > number:
+            print('Too high!')
+        elif guess < number:
+            print('Too low!')
+        else:
+            print(f'Congratulations! You have guessed the secret number: {number}')
+            break
+        
+        num_lives -= 1
     
     if num_lives == 0:
         print(f"Better luck next time! The secret number was {number}") 
+
 game()
