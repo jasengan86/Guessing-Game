1fc998b6600206508e001896e9255d1ccef1e0be
jasengan86 <71538904+jasengan86@users.noreply.github.com>
2024-08-19 23:49:52 -0700

    Update githubGuessing.py
    added try block to catch input error.

--- githubGuessing.py
+++ githubGuessing.py
@@ -1,4 +1,4 @@
-import random
+from random import randint as r
 
 
 print("="*50)
@@ -7,30 +7,31 @@ You start out with 3 lives and you lose one with each wrong guess!""")
 print("="*50)
 print()
 
-
-number = random.randint(1,20)
-
-
-num_lives = 3
-
-while num_lives > 0:
+def game():
+    number = r(1, 30)
+    num_lives = 10
+    
+    while num_lives > 0:
     print(f"Lives remaining: {num_lives}")
-    
-    
-    guess = int(input("Please guess the secret number between 1 and 20: "))  
-    
-    
-    if guess > number:
-        print("Too high!")
-        num_lives -= 1
-    
-    elif guess < number:
-        print("Too low!")
-        num_lives -= 1
-        
-    else:
-        print("Congratulations! You guessed the secret number", number) 
-        break  
 
-if num_lives == 0:
-    print(f"Better luck next time! The secret number was {number}") 
+    try:
+        guess = int(input('Please guess the secret number between 1 and 30: '))
+    except ValueError:
+        print('Invalid range. Please choose 1 - 30')
+        continue
+
+    if guess < 0 or guess > 30:
+        print('Invalid range. Please choose 1 - 30')
+        continue
+    elif guess > number:
+        print('Too high!')
+    elif guess < number:
+        print('Too low!')
+    else:
+        print('Congratulations! You have guessed the secret number: ', number)
+        break
+    num_lives -= 1
+    
+    if num_lives == 0:
+        print(f"Better luck next time! The secret number was {number}") 
+game()
